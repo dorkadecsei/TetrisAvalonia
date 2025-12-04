679afca4b8c41f29a8a3a6eefc7f3eaef7222148
dorkadecsei <dorka200505@gmail.com>
2025-12-04 16:57:25 +0100

    Add files via upload

---
+++ TetrisAvalonia/Tetris/Model/ITimer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Tetris.Model
+{
+    public interface ITimer
+    {
+        bool Enabled { get; set; }
+        double Interval { get; set; }
+        event EventHandler? Elapsed;
+        void Start();
+        void Stop();
+    }
+}
---
+++ TetrisAvalonia/TetrisAvalonia/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace TetrisAvalonia.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
