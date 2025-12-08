b194a7d9cf9f2102bf008cb4c7677756805cda32
dorkadecsei <dorka200505@gmail.com>
2025-12-08 10:46:41 +0100

    Add files via upload

---
+++ TetrisAvalonia/TetrisAvalonia/ViewModel/ATimer.cs
@@ -0,0 +1,37 @@
+﻿using Avalonia.Threading;
+using System;
+using System.Threading;
+using Tetris.Model;
+
+namespace TetrisAvalonia.ViewModel
+{
+    public class ATimer : Tetris.Model.ITimer
+    {
+        private readonly DispatcherTimer _timer;
+
+        public ATimer()
+        {
+            _timer = new DispatcherTimer();
+            // Az Avalonia DispatcherTimer Tick eseménye szintén EventHandler
+            _timer.Tick += (s, e) => Elapsed?.Invoke(this, EventArgs.Empty);
+        }
+
+        public bool Enabled
+        {
+            get => _timer.IsEnabled;
+            set => _timer.IsEnabled = value;
+        }
+
+        // Az Avalonia Timer Intervalja TimeSpan típusú, de az ITimer double-t (ms) vár
+        public double Interval
+        {
+            get => _timer.Interval.TotalMilliseconds;
+            set => _timer.Interval = TimeSpan.FromMilliseconds(value);
+        }
+
+        public event EventHandler? Elapsed;
+
+        public void Start() => _timer.Start();
+        public void Stop() => _timer.Stop();
+    }
+}
---
+++ TetrisAvalonia/TetrisAvalonia/ViewModel/Fields.cs
@@ -0,0 +1,68 @@
+﻿using Avalonia.Media;
+using System;
+using System.Media;
+using Tetris.Model;
+using TetrisAvalonia.ViewModel;
+
+namespace TetrisAvalonia.ViewModel
+{
+    public class Fields : ViewModelBase
+    {
+        private int _color;
+
+        /// <summary>
+        /// A mező színkódja
+        /// A Modelből érkező szám.
+        /// </summary>
+        public int Color
+        {
+            get { return _color; }
+            set
+            {
+                if (_color != value)
+                {
+                    _color = value;
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(DisplayColor));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Vízszintes koordináta
+        /// </summary>
+        public int X { get; set; }
+
+        /// <summary>
+        /// Függőleges koordináta
+        /// </summary>
+        public int Y { get; set; }
+
+        public Fields(int x, int y)
+        {
+            X = x;
+            Y = y;
+            Color = 0;
+        }
+
+        // OPCIONÁLIS: Segédtulajdonság a WPF megjelenítéshez
+        // Ha nincs Convertered XAML-ben, akkor ez fordítja le a számot valódi színre.
+        public IBrush DisplayColor
+        {
+            get
+            {
+                switch (_color)
+                {
+                    case 1: return Brushes.ForestGreen;
+                    case 2: return Brushes.Lime;
+                    case 3: return Brushes.Green;
+                    case 4: return Brushes.LawnGreen;
+                    case 5: return Brushes.LightGreen;
+                    case 6: return Brushes.DarkGreen;
+                    case 7: return Brushes.SpringGreen;
+                    default: return Brushes.White;
+                }
+            }
+        }
+    }
+}
